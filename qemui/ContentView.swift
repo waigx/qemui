db7fef9a75b59dd3fae716099ed075bb3a3eaf0f
Yigong Wang <yigwang@cs.stonybrook.edu>
2020-04-13 21:19:09 +0100

    Initial Commit

---
+++ qemui/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  qemui
+//
+//  Created by Igor Wang on 4/13/20.
+//  Copyright © 2020 Yigong Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
